cdba410ec446069d65772a74495422e33390c42d
okomeee <marukome.diary@gmail.com>
2019-05-26 12:00:38 +0900

    conflict

--- lib/pages/title.dart
+++ lib/pages/title.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'dart:ui' as ui;
 
+
 class TitlePage extends StatefulWidget {
   @override
   _TitleState createState() => new _TitleState();
@@ -8,14 +9,13 @@ class TitlePage extends StatefulWidget {
 
 class _TitleState extends State<TitlePage> {
   final phraseController = TextEditingController();
-  String dropdownValue = 'E ☆ Hoshinari';
   @override
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
     //var listItem = ["one", "two", "three"];
     return new Scaffold(
       body: Stack(
-        children: <Widget>[
+        children:<Widget>[
           Center(
             child: Image.asset(
               'images/wasi.png',
@@ -30,53 +30,43 @@ class _TitleState extends State<TitlePage> {
             ),
           ),
           Container(
-              padding: new EdgeInsets.all(50.0),
-              child: new Form(
-                child: new ListView(
-                  children: <Widget>[
-                    Image.asset('images/wakaiwa.png'),
-                    // ListView.builder(
-                    //   itemBuilder: (BuildContext context, int index) {
-                    //     return Card(
-                    //       child: Padding(
-                    //           child: Text('$index', style: TextStyle(fontSize: 22.0),),
-                    //           padding: EdgeInsets.all(20.0),),
-                    //       );
-                    //     },
-                    //   itemCount: listItem.length,
-                    // ),
-                    DropdownButton<String>(
-                      value: dropdownValue,
-                      onChanged: (String newValue) {
-                        setState(() {
-                          dropdownValue = newValue;
-                        });
-                      },
-                      items: <String>['E ☆ Hoshinari', 'K 米 〇kome']
-                          .map<DropdownMenuItem<String>>((String value) {
-                        return DropdownMenuItem<String>(
-                          value: value,
-                          child: Text(value),
-                        );
-                      }).toList(),
-                    ),
-                    Container(
-                      width: size.width,
-                      child: new RaisedButton(
-                        child: new Text(
-                          '変換',
-                          style: new TextStyle(color: Colors.white),
+            padding: new EdgeInsets.all(50.0),
+            child: new Form(
+              child: new ListView(
+                children: <Widget>[
+                  Image.asset('images/wakaiwa.png'),
+                  // ListView.builder(
+                  //   itemBuilder: (BuildContext context, int index) {
+                  //     return Card(
+                  //       child: Padding(
+                  //           child: Text('$index', style: TextStyle(fontSize: 22.0),),
+                  //           padding: EdgeInsets.all(20.0),),
+                  //       );
+                  //     },
+                  //   itemCount: listItem.length,
+                  // ),
+                  Container(
+                    width: size.width,
+                    child: new RaisedButton(
+                      child: new Text(
+                        '変換',
+                        style: new TextStyle(
+                          color: Colors.white
                         ),
-                        onPressed: () {
-                          Navigator.pushNamed(context, '/result');
-                        },
-                        color: Colors.lime[700],
                       ),
-                      margin: new EdgeInsets.only(top: 20.0),
+                      onPressed: (){
+                        Navigator.pushNamed(context, '/result');
+                      },
+                      color: Colors.lime[700],
                     ),
-                  ],
-                ),
-              )),
+                    margin: new EdgeInsets.only(
+                      top: 20.0
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ),
         ],
       ),
     );
