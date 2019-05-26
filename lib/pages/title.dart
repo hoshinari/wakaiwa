4ce2a78fbfb79023aaad6ee87930939160354cd6
okomeee <marukome.diary@gmail.com>
2019-05-26 12:12:09 +0900

    conflicts

--- lib/pages/title.dart
+++ lib/pages/title.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:ui' as ui;
 
-
 class TitlePage extends StatefulWidget {
   @override
   _TitleState createState() => new _TitleState();
@@ -9,13 +8,14 @@ class TitlePage extends StatefulWidget {
 
 class _TitleState extends State<TitlePage> {
   final phraseController = TextEditingController();
+  String dropdownValue = 'E ☆ Hoshinari';
   @override
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
     //var listItem = ["one", "two", "three"];
     return new Scaffold(
       body: Stack(
-        children:<Widget>[
+        children: <Widget>[
           Center(
             child: Image.asset(
               'images/wasi.png',
@@ -30,30 +30,6 @@ class _TitleState extends State<TitlePage> {
             ),
           ),
           Container(
-<<<<<<< HEAD
-            padding: new EdgeInsets.all(50.0),
-            child: new Form(
-              child: new ListView(
-                children: <Widget>[
-                  Image.asset('images/wakaiwa.png'),
-                  // ListView.builder(
-                  //   itemBuilder: (BuildContext context, int index) {
-                  //     return Card(
-                  //       child: Padding(
-                  //           child: Text('$index', style: TextStyle(fontSize: 22.0),),
-                  //           padding: EdgeInsets.all(20.0),),
-                  //       );
-                  //     },
-                  //   itemCount: listItem.length,
-                  // ),
-                  Container(
-                    width: size.width,
-                    child: new RaisedButton(
-                      child: new Text(
-                        '変換',
-                        style: new TextStyle(
-                          color: Colors.white
-=======
               padding: new EdgeInsets.all(50.0),
               child: new Form(
                 child: new ListView(
@@ -87,22 +63,17 @@ class _TitleState extends State<TitlePage> {
                         child: new Text(
                           '言葉を生成する',
                           style: new TextStyle(color: Colors.white),
->>>>>>> b09ec252256acb33f27d9602473fd8ebbc0a6b56
                         ),
+                        onPressed: () {
+                          Navigator.pushNamed(context, '/result');
+                        },
+                        color: Colors.lime[700],
                       ),
-                      onPressed: (){
-                        Navigator.pushNamed(context, '/result');
-                      },
-                      color: Colors.lime[700],
+                      margin: new EdgeInsets.only(top: 20.0),
                     ),
-                    margin: new EdgeInsets.only(
-                      top: 20.0
-                    ),
-                  ),
-                ],
-              ),
-            )
-          ),
+                  ],
+                ),
+              )),
         ],
       ),
     );
